05b309fa23c16df92e819e17e26ac979ec202f4f
sebgoa <runseb@gmail.com>
2019-01-29 22:35:42 +0100

    tweaks

--- knative/controller.py
+++ knative/controller.py
@@ -10,19 +10,20 @@ from cloudevents.sdk import marshaller
 from cloudevents.sdk.converters import structured
 from cloudevents.sdk.event import v01
 
-channel = os.environ['CHANNEL']
-namespace = os.environ['NAMESPACE']
 gcp_project = os.environ['PROJECT']
 gcs_subscription = os.environ['SUBSCRIPTION']
+google_credentials = os.environ['CREDENTIALS']
+google_credentials_path = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+sink_url = os.environ['SINK']
 
-sub = pubsub_v1.SubscriberClient()
-
-sub_name = 'projects/%s/subscriptions/%s' % (gcp_project, gcs_subscription)
+def write_credentials():
+  fh=open(google_credentials_path, 'w')
+  fh.write(str(google_credentials))
+  fh.close() 
 
 def callback(message):
     print(message.data)
-    url = 'http://%s-channel.%s.svc.cluster.local' % (channel, namespace)
-    print(url)
+    print(sink_url)
 
     event = (
         v01.Event().
@@ -41,8 +42,12 @@ def callback(message):
 
     headers, body = m.ToRequest(event, converters.TypeStructured, lambda x: x)
 
-    requests.post(url, data=body, headers=headers)
+    requests.post(sink_url, data=body, headers=headers)
     message.ack()
+
+write_credentials()
+sub = pubsub_v1.SubscriberClient()
+sub_name = 'projects/%s/subscriptions/%s' % (gcp_project, gcs_subscription)
 
 future = sub.subscribe(sub_name, callback)
 future.result()
